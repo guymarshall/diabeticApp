23c010bebee648bfeb2a5685df6b0779a3c3b333
Guy Marshall <guymarshall9@gmail.com>
2022-04-22 22:27:06 +0100

    Add upload page

---
+++ upload.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button name="back" id="back" onclick="location.href = 'index.php';">Back</button>
+    <form name="glucoseForm" id="glucoseForm" action="" method="post">
+        <?php
+            //time (default now)
+            //date (default now)
+            //blood glucose
+            //carbs (default 0)
+            //insulin short (default 0)
+            //insulin long (default 0)
+            //insulin correction (default 0)
+            //notes
+        ?>
+    </form>
+</body>
+</html>
