7f6f53197d2c03761b3a2a928e650185f428f622
Guy Marshall <guymarshall9@gmail.com>
2022-04-23 15:53:55 +0100

    Add input fields + labels

--- add.php
+++ add.php
@@ -20,6 +20,31 @@
 <body>
     <button name="back" id="back" onclick="location.href = 'index.php';">Back</button>
     <form name="glucoseForm" id="glucoseForm" action="" method="post">
+    <label for="time">time</label>    
+    <input type="time" name="time" id="time" value="<?php echo date('H:i'); ?>">
+        <br>
+        <label for="date">date</label>
+        <input type="date" name="date" id="date" value="<?php echo date('Y-m-d'); ?>">
+        <br>
+        <label for="bloodGlucose">bloodGlucose</label>
+        <input type="number" name="bloodGlucose" id="bloodGlucose">
+        <br>
+        <label for="carbs">carbs</label>
+        <input type="number" name="carbs" id="carbs">
+        <br>
+        <label for="insulinShort">insulinShort</label>
+        <input type="number" name="insulinShort" id="insulinShort">
+        <br>
+        <label for="insulinLong">insulinLong</label>
+        <input type="number" name="insulinLong" id="insulinLong">
+        <br>
+        <label for="insulinCorrection">insulinCorrection</label>
+        <input type="number" name="insulinCorrection" id="insulinCorrection">
+        <br>
+        <label for="notes">notes</label>
+        <textarea name="notes" id="notes" cols="30" rows="10" placeholder="Enter notes..."></textarea>
+        <br>
+        <button type="submit">Add</button>
     </form>
 </body>
 </html>
