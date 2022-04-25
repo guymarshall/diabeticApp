4ca33a85c8e9092947ec316f12dc41fa0f175cdf
Guy Marshall <guy@cslgroupservices.co.uk>
2022-04-25 12:40:37 +0100

    Add login fields

--- login.php
+++ login.php
@@ -10,6 +10,12 @@
 <body>
 	<button name="back" id="back" onclick="location.href = 'index.php';">Back</button>
 	<h1>Login</h1>
-	
+	<form action="login.php" method="post">
+        <label for="email">Email Address:</label>
+        <input type="email" name="email" id="email" placeholder="Email Address...">
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" placeholder="Password...">
+    </form>
 </body>
 </html>
