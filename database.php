e8477ee3b2ecb15073fdc8720a35718f5d3eaea5
Guy Marshall <guymarshall9@gmail.com>
2022-05-01 12:11:28 +0100

    Add try catch for database (ini file fixed - mysqli works now)

--- database.php
+++ database.php
@@ -3,19 +3,36 @@ class Database {
 	public $servername;
 	public $username;
 	public $password;
+	public $database;
 
 	public function __construct() {
 		$this->servername = "localhost";
 		$this->username = "username";
 		$this->password = "password";
+		$this->database = "database";
 	}
 
 	public function create_and_check_connection() {
-		$connection = new mysqli($servername, $username, $password);
-		if ($connection->connect_error) {
-				die("Connection failed: " . $connection->connect_error);
+		echo "test<br>";
+		try {
+			$connection = new mysqli($servername, $username, $password, $database);
+			echo "test<br>";
 		}
-		echo "Connected successfully";
+		catch (Throwable $t) {
+			echo $t;
+			echo 'in catch<br>';
+			echo "an error has occured<br>";
+			if ($connection->connect_error) {
+				echo "Connection failed: " . $connection->connect_error;
+				die();
+			}
+		}
+		finally {
+			echo "finally bit<br>";
+		}
+
+		echo "Connected successfully<br>";
+		echo 'another<br>';
 	}
 }
 ?>
