0faf3e76c8862b47851f0afcb9b25c1e10bab89d
Guy Marshall <guymarshall.uk@gmail.com>
2022-05-31 22:07:28 +0100

    Implement add.php properly

---
+++ pages/add.php
@@ -0,0 +1,46 @@
+<?php
+
+require 'page.php';
+
+$content = '';
+
+$content .= '
+<form name="glucoseForm" id="glucoseForm" action="add.php" method="post">
+	<label class="col-sm-2" for="time">time</label>
+	<input class="form-control" type="time" name="time" id="time" value="'.date('H:i').'">
+	<label class="col-sm-2" for="date">date</label>
+	<input class="form-control" type="date" name="date" id="date" value="'.date('Y-m-d').'">
+	<label class="col-sm-2" for="bloodGlucose">bloodGlucose</label>
+	<input class="form-control" type="number" name="bloodGlucose" id="bloodGlucose" min="0.0" step="0.1">
+	<label class="col-sm-2" for="carbs">carbs</label>
+	<input class="form-control" type="number" name="carbs" id="carbs" min="0">
+	<label class="col-sm-2" for="insulinShort">insulinShort</label>
+	<input class="form-control" type="number" name="insulinShort" id="insulinShort" min="0">
+	<label class="col-sm-2" for="insulinLong">insulinLong</label>
+	<input class="form-control" type="number" name="insulinLong" id="insulinLong" min="0">
+	<label class="col-sm-2" for="insulinCorrection">insulinCorrection</label>
+	<input class="form-control" type="number" name="insulinCorrection" id="insulinCorrection" min="0">
+	<label class="col-sm-2" for="notes">notes</label>
+	<textarea class="form-control" name="notes" id="notes" cols="30" rows="10" placeholder="Enter notes..."></textarea>
+	<button type="submit">Add</button>
+</form>
+';
+
+$user_input = [
+	'time' => $time = isset($_POST['time']) ? $_POST['time'] : '',
+	'date' => $date = isset($_POST['date']) ? $_POST['date'] : '',
+	'bloodGlucose' => $bloodGlucose = isset($_POST['bloodGlucose']) ? filter_var($_POST['bloodGlucose'], FILTER_SANITIZE_NUMBER_FLOAT) : '',
+	'carbs' => $carbs = isset($_POST['carbs']) ? filter_var($_POST['carbs'], FILTER_SANITIZE_NUMBER_INT) : '',
+	'insulinShort' => $insulinShort = isset($_POST['insulinShort']) ? filter_var($_POST['insulinShort'], FILTER_SANITIZE_NUMBER_INT) : '',
+	'insulinLong' => $insulinLong = isset($_POST['insulinLong']) ? filter_var($_POST['insulinLong'], FILTER_SANITIZE_NUMBER_INT) : '',
+	'insulinCorrection' => $insulinCorrection = isset($_POST['insulinCorrection']) ? filter_var($_POST['insulinCorrection'], FILTER_SANITIZE_NUMBER_INT) : '',
+	'notes' => $notes = isset($_POST['notes']) ? $_POST['notes'] : ''
+];
+
+// echo '<pre>';
+// print_r($user_input);
+// echo '</pre>';
+
+$o_add_page = new Page();
+$add_html = $o_add_page->create_page('Add', $content);
+echo $add_html;
